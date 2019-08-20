292f480bbfdce4316ae0b62c46f3f2533cf38277
RongSon <shrimp509@hotmail.com>
2019-08-20 22:22:39 +0800

    Add main image for switching on or off

--- app/src/main/java/net/rongsonho/brightnessking/MainActivity.kt
+++ app/src/main/java/net/rongsonho/brightnessking/MainActivity.kt
@@ -2,11 +2,26 @@ package net.rongsonho.brightnessking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn : ImageButton
+    var isActivate : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.main_btn)
+
+        btn.setOnClickListener{
+            isActivate = !isActivate
+            if (isActivate){
+                btn.setImageResource(R.drawable.btn_on)
+            }else{
+                btn.setImageResource(R.drawable.btn_off)
+            }
+        }
     }
 }
