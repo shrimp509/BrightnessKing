47f0600e94e24b52b912667e6525dfd59320051d
RongSon <shrimp509@hotmail.com>
2019-08-24 09:57:14 +0800

    Change main appearance - cancel action bar - add title and change typeface - add logo animation

--- app/src/main/java/net/rongsonho/brightnessking/MainActivity.kt
+++ app/src/main/java/net/rongsonho/brightnessking/MainActivity.kt
@@ -1,8 +1,12 @@
 package net.rongsonho.brightnessking
 
+import android.animation.Animator
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn : ImageButton
@@ -12,8 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn = findViewById(R.id.main_btn)
+        initView()
+    }
 
+    private fun initView(){
+        initBrightnessOnOffButton()
+
+        setLogoAnimation()
+
+        setTitleTypeFace()
+    }
+
+    private fun initBrightnessOnOffButton(){
+
+        // TODO: use shared preference
+        btn = findViewById(R.id.main_btn)
         btn.setOnClickListener{
             isActivate = !isActivate
             if (isActivate){
@@ -22,5 +39,42 @@ class MainActivity : AppCompatActivity() {
                 btn.setImageResource(R.drawable.button_off_state)
             }
         }
+    }
+
+    private fun setLogoAnimation(){
+
+        // set animation
+        val backgroundWhite = findViewById<ImageView>(R.id.white_background)
+        val logo = findViewById<ImageView>(R.id.main_icon)
+
+        logo.animate().alpha(1f).setDuration(2000).setListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(p0: Animator?) {
+                // Nothing to do
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+
+                // set Foreground icon and background to
+                backgroundWhite.animate().alpha(0f).setDuration(300).start()
+                logo.animate().alpha(0f).setDuration(300).start()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+                // Nothing to do
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+                // Nothing to do
+            }
+        }).start()
+    }
+
+    private fun setTitleTypeFace(){
+        val title = findViewById<TextView>(R.id.main_title)
+        setTypeFace(title)
+    }
+
+    private fun setTypeFace(view : TextView){
+        view.typeface = Typeface.createFromAsset(assets, resources.getString(R.string.main_title_typeface))
     }
 }
